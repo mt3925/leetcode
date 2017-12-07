de3c8345cbcce6dfca24a958f1f4486cc725f745
mat <mat@pv.cc>
2017-12-07 16:38:04 +0800

    update format

--- Python/two_sum.py
+++ Python/two_sum.py
@@ -1,7 +1,9 @@
 """
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Given an array of integers, return indices of the two numbers such that they
+ add up to a specific target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, and you may
+ not use the same element twice.
 
 Example:
 Given nums = [2, 7, 11, 15], target = 9,
@@ -9,6 +11,8 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
+
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
