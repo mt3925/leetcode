4cf8d08a04c78a10f6516ac30f99a54156b024f0
mat <mat@pv.cc>
2017-12-07 15:53:16 +0800

    update add two numbers

--- Python/add_two_numbers.py
+++ Python/add_two_numbers.py
@@ -9,11 +9,25 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 """
+
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def add_next(self, x):
+        self.next = x
+        return self
+
+    def __str__(self):
+        n = self
+        s = ''
+        while n:
+            s += str(n.val)
+            n = n.next
+        return s
+
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -22,26 +36,24 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        def _add(node, v):
-            _x, y = divmod(v, 10)
-            node.val = y
-            return _x
-        
-        _a, a, b = l1, l1, l2
-        x = 0
-        while a is not None and b is not None:
-            _a, x, a, b = a, _add(a, a.val + b.val + x), a.next, b.next
-        while a is not None and x is not 0:
-            _a, x, a = a, _add(a, a.val + x), a.next
-        while b is not None:
-            if _a is None:
-                _a = l1 = b
-            else:
-                _a.next = b
-                _a = b
-            x, b = _add(b, b.val + x), b.next
-            if x is 0:
+        rtn, carry = l1, 0
+        pre = cur = rtn
+        while l1 and l2:
+            carry, cur.val = divmod(l1.val + l2.val + carry, 10)
+            l1, l2, pre, cur = l1.next, l2.next, cur, cur.next
+        if l2:
+            pre.next = cur = l2
+        while carry:
+            if not cur:
+                pre.next = ListNode(carry)
                 break
-        if x is not 0:
-            _a.next = ListNode(x)
-        return l1
+            carry, cur.val = divmod(cur.val + carry, 10)
+            pre, cur = cur, cur.next
+        return rtn
+
+
+if __name__ == '__main__':
+    l1 = ListNode(1)
+    l2 = ListNode(9).add_next(ListNode(9))
+    print l1, l2
+    print Solution().addTwoNumbers(l1, l2)
